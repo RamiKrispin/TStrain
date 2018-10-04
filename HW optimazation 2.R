94c389141a30abfc1458b306ac80a3c528d3ed45
RamiKrispin <ramkrisp@umich.edu>
2018-10-03 20:38:40 -0700

    Added rmse grid search

--- HW optimazation 2.R
+++ HW optimazation 2.R
@@ -2,9 +2,9 @@ library(TSstudio)
 data(USgas)
 data(USVSales)
 h <- 12
-periods <- 3
+periods <- 6
 window_size <- h
-window_space <- h
+window_space <- 1
 ts_partition <- ts_split(USVSales, sample.out = h)
 
 valid <- ts_partition$test
@@ -27,7 +27,7 @@ names(temp) <- base::paste0("period_", 1:periods)
 
 mape_grid <- cbind(grid_df, temp)
 mape_grid$valid <- NA
-
+rmse_grid <- mape_grid
 for(i in base::seq_along(w)){
   ts_sub <- train <- test <- c <- NULL
   ts_sub <- stats::window(ts.obj, start = stats::start(ts.obj), end = stats::time(ts.obj)[w[i]])
@@ -42,11 +42,13 @@ for(i in base::seq_along(w)){
                              gamma = mape_grid$gamma[l])
     fc <- forecast::forecast(md, h)
     mape_grid[l, c] <- forecast::accuracy(fc,test)[10]
+    rmse_grid[l, c] <- forecast::accuracy(fc,test)[4]
   }
   
   md1 <- stats::HoltWinters(train)
   fc1 <- forecast::forecast(md1, h)
   print(forecast::accuracy(fc1,test)[10])
+  print(forecast::accuracy(fc1,test)[4])
   
 }
 
@@ -58,11 +60,15 @@ for(l in 1:base::nrow(mape_grid)){
                            gamma = mape_grid$gamma[l])
   fc <- forecast::forecast(md, h)
   mape_grid$valid[l] <- forecast::accuracy(fc,valid)[10]
+  rmse_grid$valid[l] <- forecast::accuracy(fc,valid)[4]
 }
 
 
-mape_grid$avg <- (mape_grid$period_1 + mape_grid$period_2 + mape_grid$period_3 ) / periods #+
-                    # mape_grid$period_4 + mape_grid$period_5 + mape_grid$period_6) / periods
+mape_grid$avg <- (mape_grid$period_1 + mape_grid$period_2 + mape_grid$period_3 +
+                    mape_grid$period_4 + mape_grid$period_5 + mape_grid$period_6) / periods
+
+rmse_grid$avg <- (rmse_grid$period_1 + rmse_grid$period_2 + rmse_grid$period_3 +
+                    rmse_grid$period_4 + rmse_grid$period_5 + rmse_grid$period_6) / periods
 
 
 md2 <- stats::HoltWinters(ts.obj)

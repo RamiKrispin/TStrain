851c91faa43b437df194934fc256c4295e1722b0
RamiKrispin <ramkrisp@umich.edu>
2018-12-20 16:29:19 -0800

    Adding color control options

--- Forecasting simulation.R
+++ Forecasting simulation.R
@@ -1,29 +1,33 @@
 library(TSstudio)
 data("USgas")
 
-md <- forecast::auto.arima(USgas)
+md1 <- forecast::auto.arima(USgas)
+md2 <- forecast::ets(USgas)
+md3 <- forecast::tslm(USgas ~ season + trend)
+
 fc <- forecast::forecast(md, h = 60)
-ts_sim <- function(model, h, n){
+ts_sim <- function(model, h, n, sim_color = "blue", opacity = 0.05){
   s <- NULL
   s <- lapply(1:n, function(i){
     sim <- NULL
     sim <- stats::simulate(model,nsim = h)
-    sim_df <- base::data.frame(x = base::as.numeric(stats::time(sim)), y = base::as.numeric(sim))
+    sim_df <- base::data.frame(x = base::as.numeric(stats::time(sim)), 
+                               y = base::as.numeric(sim))
     sim_df$n <- paste0("test_", i)
     return(sim_df)
   }) 
-  s %>% dplyr::bind_rows() %>% 
+  sim_output <- s %>% dplyr::bind_rows() %>% 
     tidyr::spread(key = n, value = y) %>% 
     dplyr::select(-x) %>% 
     ts(start = stats::start(stats::simulate(model,nsim = 1)), 
        frequency = stats::frequency(stats::simulate(model,nsim = 1))) 
   
-  
-  
   p <- plotly::plot_ly()
   
   for(i in 1:n){
-    p <- p %>% plotly::add_lines(x = s[[i]]$x, y = s[[i]]$y, line = list(color = "blue"), opacity = 0.01, showlegend = FALSE)
+    p <- p %>% plotly::add_lines(x = s[[i]]$x, y = s[[i]]$y, 
+                                 line = list(color = sim_color), 
+                                 opacity = opacity, showlegend = FALSE)
   }
   s1 <- s %>% dplyr::bind_rows() %>% dplyr::group_by(x) %>%
     dplyr::summarise(p50 = median(y))
@@ -34,12 +38,17 @@ ts_sim <- function(model, h, n){
                                            width = 3), name = "Median") 
   
   p <- p %>% plotly::add_lines(x = time(model$x), y = model$x, line = list(color = "#00526d"), name = "Actual")
+  print(p)
   
-  return(p)
+  output <- list()
+  output$plot <- p
+  output$forecast_sim <- sim_output
+  output$series <- model$x
+  return(output)
 }
 
-p <-  ts_sim(model = md, h = 60, n = 500)
-p
+p <-  ts_sim(model = md3, h = 60, n = 50, sim_color = "blue", opacity = 0.03)
+
 p1 <- plot_forecast(fc)
 length(s)
 plotly::subplot(p, p1, nrows = 2)

a86a79346d4acc7aa4d598206eb541d5793b275e
RamiKrispin <ramkrisp@umich.edu>
2018-12-30 20:53:38 -0800

    Updated version

--- HW optimazation.R
+++ HW optimazation.R
@@ -1,234 +1,19 @@
-# Tunning the gamma parameter of Holt Winters model
-?HoltWinters
-
-# Using the USgas dataset as an example
-library(TSstudio)
-data(USgas)
-data("USVSales")
-USgas_split <- ts_split(USgas, sample.out = h)
-
-USgas_train <- USgas_split$train
-USgas_test <- USgas_split$test
-
-# Setting the window parameters
-periods <- 6
-
-window_size <- 12
-
-window_space <- 3
-
-h <- 12
-
-# Setting a sequence for the gamma parameter
-alpha <- seq(0, 1, 0.1)
-beta <- seq(0, 1, 0.1)
-gamma <- seq(0, 1, 0.1)
-alpha[1] <- alpha[1] + 0.0001
-beta[1] <- beta[1] + 0.0001
-gamma[1] <- gamma[1] + 0.0001
-
-grid_df_init_a <- base::expand.grid(alpha, beta, gamma)
-names(grid_df_init_a) <- c("alpha", "beta", "gamma")
-head(grid_df_init_a)
-tail(grid_df_init_a)
-
-s <- length(USgas_train) - window_space * (periods - 1)
-e <- length(USgas_train) 
-w <- seq(from = s, by = window_space, to = e)
-score_df <- as.data.frame(matrix(NA, 
-                                 ncol = length(w), 
-                                 nrow = nrow(grid_df_init_a)))
-
-names(score_df) <- paste0("period_", 1:length(w), sep = "")
-grid_df_init <- cbind(grid_df_init_a, score_df)
-head(grid_df_init)
-# Testing the sequance of gamma parameters over a window of 7 periods
-  
-  
-for(n in 1:length(w)){
-    ts_sub <- gamma_df <-  NULL
-    ts_sub <- stats::window(USgas_train, 
-                            start = stats::start(USgas_train), 
-                            end = stats::time(USgas_train)[w[n]])
-    partition <- TSstudio::ts_split(ts_sub, sample.out = h)
-    train <- partition$train
-    test <- partition$test
-    for(i in 1:nrow(grid_df_init)){
-      md <- fc <- NULL
-      md <- HoltWinters(train, alpha = grid_df_init$alpha[i], 
-                        beta = grid_df_init$beta[i], 
-                        gamma = grid_df_init$gamma[i])
-      fc <- forecast::forecast(md, h = h)
-      grid_df_init[i, n + 3]  <- forecast::accuracy(fc, test)[10]
-      
-    }
-    print(n)
-  }
-
-grid_df_init$mean <- (grid_df_init$period_1 + grid_df_init$period_2  + grid_df_init$period_3  + 
-                        grid_df_init$period_4 + grid_df_init$period_5 + grid_df_init$period_6) / 6
-
-
-grid_df_init <- grid_df_init %>% dplyr::arrange(mean)
-grid_init_top <- grid_df_init[1:5 , ]
-
-
-alpha1 <- seq(min(grid_init_top$alpha), max(grid_init_top$alpha), 0.01)
-beta1 <- seq(min(grid_init_top$beta), max(grid_init_top$beta), 0.01)
-gamma1 <- seq(min(grid_init_top$gamma), max(grid_init_top$gamma), 0.01)
-
-
-grid_df_second_a <- base::expand.grid(alpha1, beta1, gamma1)
-names(grid_df_second_a) <- c("alpha", "beta", "gamma")
-head(grid_df_second_a)
-tail(grid_df_second_a)
-
-score_df_second <- as.data.frame(matrix(NA, 
-                                        ncol = length(w), 
-                                        nrow = nrow(grid_df_second_a)))
-names(score_df_second) <- paste0("period_", 1:length(w), sep = "")
-grid_df_second <- cbind(grid_df_second_a, score_df_second)
-head(grid_df_second)
-# Testing the sequance of gamma parameters over a window of 7 periods
-for(n in 1:length(w)){
-  ts_sub <-  NULL
-  ts_sub <- stats::window(USgas_train, 
-                          start = stats::start(USgas_train), 
-                          end = stats::time(USgas_train)[w[n]])
-  partition <- TSstudio::ts_split(ts_sub, sample.out = h)
-  train <- partition$train
-  test <- partition$test
-  for(i in 1:nrow(grid_df_second)){
-    md <- fc <- NULL
-    md <- HoltWinters(train, alpha = grid_df_second$alpha[i], 
-                      beta = grid_df_second$beta[i], 
-                      gamma = grid_df_second$gamma[i])
-    fc <- forecast::forecast(md, h = h)
-    grid_df_second[i, n + 3]  <- forecast::accuracy(fc, test)[10]
-    
-  }
-  print(n)
-}
-
-grid_df_second$mean <- (grid_df_second$period_1 + grid_df_second$period_2  + grid_df_second$period_3  + 
-                   grid_df_second$period_4 + grid_df_second$period_5 + grid_df_second$period_6) / 6
-
-grid_df_second <- grid_df_second %>% dplyr::arrange(mean)
-
-
-
-alpha2 <- seq(min(grid_top3$alpha), max(grid_top3$alpha), 0.01)
-beta2 <- seq(min(grid_top3$beta), max(grid_top3$beta), 0.01)
-gamma2 <- seq(min(grid_top3$gamma), max(grid_top3$gamma), 0.01)
-
-
-grid_df4 <- base::expand.grid(alpha2, beta2, gamma2)
-names(grid_df4) <- c("alpha", "beta", "gamma")
-head(grid_df4)
-tail(grid_df4)
-
-score_df4 <- as.data.frame(matrix(NA, ncol = length(w), nrow = nrow(grid_df4)))
-names(score_df4) <- paste0("period_", 1:length(w), sep = "")
-grid_df5 <- cbind(grid_df4, score_df4)
-head(grid_df5)
-# Testing the sequance of gamma parameters over a window of 7 periods
-
-
-for(n in 1:length(w)){
-  ts_sub <- gamma_df <-  NULL
-  ts_sub <- stats::window(USgas_train, 
-                          start = stats::start(USgas_train), 
-                          end = stats::time(USgas_train)[w[n]])
-  partition <- TSstudio::ts_split(ts_sub, sample.out = h)
-  train <- partition$train
-  test <- partition$test
-  for(i in 1:nrow(grid_df5)){
-    md <- fc <- NULL
-    md <- HoltWinters(train, alpha = grid_df5$alpha[i], 
-                      beta = grid_df5$beta[i], 
-                      gamma = grid_df5$gamma[i])
-    fc <- forecast::forecast(md, h = h)
-    grid_df5[i, n + 3]  <- forecast::accuracy(fc, test)[10]
-    
-  }
-  print(n)
-}
-
-grid_df5$mean <- (grid_df5$period_1 + grid_df5$period_2  + grid_df5$period_3  + 
-                    grid_df5$period_4 + grid_df5$period_5 + grid_df5$period_6) / 6
-
-
-
-r <- which.min(grid_df_second$mean)
-
-
-md <- HoltWinters(USgas_train, alpha = grid_df_second$alpha[r], 
-                  beta = grid_df_second$beta[r], 
-                  gamma = grid_df_second$gamma[r])
-fc <- forecast::forecast(md, h = h)
-forecast::accuracy(fc, USgas_test)
-
-md1 <- HoltWinters(USgas_train)
-fc1 <- forecast::forecast(md1, h = h)
-forecast::accuracy(fc1, USgas_test)
-
-holt_output <- lapply(w, function(i){
-  ts_sub <- gamma_df <-  NULL
-  ts_sub <- stats::window(ts, start = stats::start(ts), end = stats::time(ts)[i])
-  partition <- TSstudio::ts_split(ts_sub, sample.out = h)
-  train <- partition$train
-  test <- partition$test
-  gamma_df <- data.frame(gamma = gamma, 
-                         MAPE = rep(NA, length(gamma)), 
-                         RMSE = rep(NA, length(gamma)))
-  for(i in 1:nrow(grid_df)){
-    md <- HoltWinters(train, alpha = grid_df$alpha[i], 
-                      beta = grid_df$beta[i], 
-                      gamma = grid_df$gamma[i])
-    fc <- forecast::forecast(md, h = h)
-    gamma_df$MAPE[i]  <- forecast::accuracy(fc, test)[10]
-    gamma_df$RMSE[i] <- forecast::accuracy(fc, test)[4]
-    
-  }
-  return(gamma_df)
-}
-)
-
-p_mape <- plotly::plot_ly()
-for(i in 1:length(holt_output)){
-  p_mape <- p_mape %>%plotly::add_lines(x = holt_output[[i]]$gamma, y = holt_output[[i]]$MAPE, name = i)
-}
-p_mape <- p_mape %>% plotly::layout(title = "MAPE as function of gamma", 
-                          yaxis = list(title = "MAPE"),
-                          xaxis = list(title = "gamma"))
-
-p_mape
-
-
-p_rmse <- plotly::plot_ly()
-for(i in 1:length(holt_output)){
-  p_rmse <- p_rmse %>%plotly::add_lines(x = holt_output[[i]]$gamma, y = holt_output[[i]]$RMSE, name = i)
-}
-p_rmse <- p_rmse %>% plotly::layout(title = "RMSE as function of gamma", 
-                          yaxis = list(title = "RMSE"),
-                          xaxis = list(title = "gamma"))
-p_rmse
-
-md2 <- HoltWinters(ts, alpha = alpha, beta = beta, gamma = 0.51)
-fc2 <- forecast::forecast(md2, h = h)
-TSstudio::plot_forecast(fc2)
-
-periods <- 6 # Number of testing periods
-window_space <- 6 # Space between each testing partition
-window_length <- 36 # Length of training window for sliding backtesting
-window_test <- 12 # Length of the testing partition
-models <- "abehntwp"  # Type of models
-window_type <- "both" # Type of backtesting window 
-
-
-hyper_params <- list(alpha = seq(0,1, 0.1), 
-                     beta = seq(0,1, 0.1), 
-                     gamma  = seq(0,1, 0.1))
+#' Tuning Time Series Forecasting Models Parameters with Grid Search 
+#' @export ts_grid
+#' @param ts.obj A univariate time series object of a class "ts"
+#' @param model A string, defines the model
+#' @param periods A string, set the number backtesting periods
+#' @param window_length An integer, defines the length of the backtesting training window.
+#' If set to NULL (default) will use an expending window starting the from the first observation,
+#'  otherwise will use a sliding window.
+#' @param window_space An integer, set the space length between each of the backtesting training partition 
+#' @param window_test An integer, set the length of the backtesting testing partition
+#' @param hyper_params A list, defines the tuning parameters and their range
+#' @param parallel Logical, if TRUE use multiple cores in parallel
+#' @param n.cores Set the number of cores to use if the parallel argument is set to TRUE. 
+#' @description Tuning time series models with grid serach approach using backtesting method.
+#'  If set to "auto" (default), will use all available cores in the system minus 1
+#'  @return A list
 
 ts_grid <- function(ts.obj, 
                     model, 
@@ -237,16 +22,20 @@ ts_grid <- function(ts.obj,
                     window_space,
                     window_test,
                     hyper_params,
-                    search_criteria, 
                     parallel = TRUE,
                     n.cores = "auto"){
- 
+  
+  mape <- period <- start_time <-  NULL
+  
+  `%>%` <- magrittr::`%>%` 
+  
   # Error handling
   if(!stats::is.ts(ts.obj)){
     stop("The input object is not 'ts' object")
   } else if(stats::is.mts(ts.obj)){
     stop("The input object is 'mts' object, please use 'ts'")
   }
+  
   
   if(!base::is.logical(parallel)){
     warning("The 'parallel' argument is not a boolean operator, setting it to TRUE")
@@ -274,9 +63,12 @@ ts_grid <- function(ts.obj,
   }
   
   if(n.cores == "auto"){
-    n.cores <- base::as.numeric( future::availableCores() - 1)
+    n.cores <- base::as.numeric(future::availableCores() - 1)
   }
-  if(!model %in% c("HoltWinters")){
+  
+  if(!base::exists("model")){
+    stop("The 'model' argument is missing")
+  } else if(!model %in% c("HoltWinters")){
     stop("The 'model' argument is not valid")
   }
   
@@ -356,123 +148,93 @@ ts_grid <- function(ts.obj,
                                      sep = "")))
     base::names(grid_df) <- c(base::names(hyper_params))
     
-   grid_model <- base::paste("stats::HoltWinters(x = train", sep = "")
-  for(i in hw_par){
-    if(i %in% base::names(grid_df)){
-      grid_model <- base::paste(grid_model, ", ", i, " = search_df$", i, "[i]", 
-                              sep = "" )
-    } else {
-      grid_model <- base::paste(grid_model, ", ", i, " = NULL", sep = "")
+    grid_model <- base::paste("stats::HoltWinters(x = train", sep = "")
+    for(i in hw_par){
+      if(i %in% base::names(grid_df)){
+        grid_model <- base::paste(grid_model, ", ", i, " = search_df$", i, "[i]", 
+                                  sep = "" )
+      } else {
+        grid_model <- base::paste(grid_model, ", ", i, " = NULL", sep = "")
+      }
     }
-  }
-   grid_model <- base::paste(grid_model, ")", sep = "")
-   }
-  
-
-  
-  
-grid_output <- NULL
-if(!parallel){
-  start_time <- Sys.time()
-grid_output <- base::lapply(1:periods, function(n){
-  ts_sub <- train <- test <- search_df <- NULL
-  
-  search_df <- grid_df
-  search_df$period <- n
-  search_df$mape <- NA
-  ts_sub <- stats::window(ts.obj, 
-                          start = stats::time(ts.obj)[w_start[n]], 
-                          end = stats::time(ts.obj)[w_end[n]])
-  partition <- TSstudio::ts_split(ts_sub, sample.out = window_test)
-  train <- partition$train
-  test <- partition$test
-  
-  for(i in 1:nrow(search_df)){
-    md <- fc <- NULL
-    md <- base::eval(base::parse(text = grid_model))
-    fc <- forecast::forecast(md, h = window_test)
-    search_df$mape[i] <- forecast::accuracy(fc, test)[10]
+    grid_model <- base::paste(grid_model, ")", sep = "")
   }
   
-  return(search_df)
-  }) %>% 
-  dplyr::bind_rows() %>%
-  tidyr::spread(key = period, value = mape)
-end <- Sys.time() - start_time
-end
-} else if(parallel){
-future::plan(future::multiprocess, workers = 8) 
-start_time <- Sys.time()
-grid_output <- future.apply::future_lapply(1:periods, function(n){
-  ts_sub <- train <- test <- search_df <- NULL
   
-  search_df <- grid_df
-  search_df$period <- n
-  search_df$mape <- NA
-  ts_sub <- stats::window(ts.obj, 
-                          start = stats::time(ts.obj)[w_start[n]], 
-                          end = stats::time(ts.obj)[w_end[n]])
-  partition <- TSstudio::ts_split(ts_sub, sample.out = window_test)
-  train <- partition$train
-  test <- partition$test
-  
-  for(i in 1:nrow(search_df)){
-    md <- fc <- NULL
-    md <- base::eval(base::parse(text = grid_model))
-    fc <- forecast::forecast(md, h = window_test)
-    search_df$mape[i] <- forecast::accuracy(fc, test)[10]
+  grid_output <- NULL
+  if(!parallel){
+    grid_output <- base::lapply(1:periods, function(n){
+      ts_sub <- train <- test <- search_df <- NULL
+      
+      search_df <- grid_df
+      search_df$period <- n
+      search_df$mape <- NA
+      ts_sub <- stats::window(ts.obj, 
+                              start = stats::time(ts.obj)[w_start[n]], 
+                              end = stats::time(ts.obj)[w_end[n]])
+      partition <- TSstudio::ts_split(ts_sub, sample.out = window_test)
+      train <- partition$train
+      test <- partition$test
+      
+      for(i in 1:nrow(search_df)){
+        md <- fc <- NULL
+        md <- base::eval(base::parse(text = grid_model))
+        fc <- forecast::forecast(md, h = window_test)
+        search_df$mape[i] <- forecast::accuracy(fc, test)[10]
+      }
+      
+      return(search_df)
+    }) %>% 
+      dplyr::bind_rows() %>%
+      tidyr::spread(key = period, value = mape)
+  } else if(parallel){
+    future::plan(future::multiprocess, workers = n.cores)  
+    start_time <- Sys.time()
+    grid_output <- future.apply::future_lapply(1:periods, function(n){
+      ts_sub <- train <- test <- search_df <- NULL
+      
+      search_df <- grid_df
+      search_df$period <- n
+      search_df$mape <- NA
+      ts_sub <- stats::window(ts.obj, 
+                              start = stats::time(ts.obj)[w_start[n]], 
+                              end = stats::time(ts.obj)[w_end[n]])
+      partition <- TSstudio::ts_split(ts_sub, sample.out = window_test)
+      train <- partition$train
+      test <- partition$test
+      
+      for(i in 1:nrow(search_df)){
+        md <- fc <- NULL
+        md <- base::eval(base::parse(text = grid_model))
+        fc <- forecast::forecast(md, h = window_test)
+        search_df$mape[i] <- forecast::accuracy(fc, test)[10]
+      }
+      
+      return(search_df)
+    }) %>% 
+      dplyr::bind_rows() %>%
+      tidyr::spread(key = period, value = mape)
   }
   
-  return(search_df)
-}) %>% 
-  dplyr::bind_rows() %>%
-  tidyr::spread(key = period, value = mape)
-end <- Sys.time() - start_time
-end
-}
-
-col_mean <- base::which(!base::names(grid_output)  %in% base::names(hyper_params) )
-grid_output$mean <- base::rowMeans(grid_output[, col_mean])
-grid_output <- grid_output %>% dplyr::arrange(mean)
-
-final_output <- NULL
-final_output <- list(grid_df = grid_output)
-
-for(i in base::names(hyper_params)){
-  final_output[[i]] <- grid_output[1, i]
-}
-
-
-return(grid_output)
-}
-
-
-grid_output1 <- grid_output[1:10, ] 
-
-
-hyper_params <- list(alpha = seq(min(grid_output1$alpha),max(grid_output1$alpha), 0.01), 
-                     beta = seq(min(grid_output1$beta), max(grid_output1$beta), 0.01), 
-                     gamma  = seq(min(grid_output1$gamma), max(grid_output1$gamma), 0.01))
-
-
-md <- HoltWinters(USgas_train, alpha = grid_output$alpha[1], 
-                  beta = grid_output$beta[1], 
-                  gamma = grid_output$gamma[1])
-fc <- forecast::forecast(md, h = h)
-forecast::accuracy(fc, USgas_test)
-
-md1 <- HoltWinters(USgas_train)
-fc1 <- forecast::forecast(md1, h = window_test)
-forecast::accuracy(fc1, USgas_test)
-
-
-final_output <- list(grid_df = grid_output,
-                     alpha = grid_output$alpha[1],
-                     beta = grid_output$beta[1],
-                     gamma = grid_output$gamma[1])
-
-
-
-for(i in base::names(hyper_params)){
-  final_output[[i]] <- grid_output[1, i]
+  col_mean <- base::which(!base::names(grid_output)  %in% base::names(hyper_params) )
+  grid_output$mean <- base::rowMeans(grid_output[, col_mean])
+  grid_output <- grid_output %>% dplyr::arrange(mean)
+  
+  final_output <- list(grid_df = grid_output)
+  
+  for(i in base::names(hyper_params)){
+    final_output[[i]] <- grid_output[1, i]
+  }
+  final_output[["parameters"]] <- list(series = ts.obj, 
+                                       model = model, 
+                                       periods = periods,
+                                       window_length = window_length, 
+                                       window_space = window_space,
+                                       window_test = window_test,
+                                       hyper_params = hyper_params,
+                                       parallel = parallel,
+                                       n.cores = n.cores)
+  
+  base::class(final_output) <- "ts_grid" 
+  return(final_output)
 }

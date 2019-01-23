a8139ace70335ff4e7254fb875407dc50642f25a
RamiKrispin <ramkrisp@umich.edu>
2019-01-23 09:31:15 -0800

    ARIMA model with plumber API

---
+++ Tuning ARIMA with plumber.R
@@ -0,0 +1,37 @@
+# plumber.R
+
+#-------------------------------------------------------
+# To call the API use
+# pr <- plumber::plumb("Tuning ARIMA with plumber.R")
+# pr$run()
+# Open the swagger UI on your browser
+#-------------------------------------------------------
+
+#' Forecast with ARIMA model the AirPassenger dataset
+#' @param p:int
+#' @param d:int
+#' @param q:int
+#' @param P:int
+#' @param D:int
+#' @param Q:int
+#' @param h:int
+#' @get /plot
+#' @png
+function(p = 0, d = 0, q = 0, P = 0, D = 0 ,Q = 0, h = 12){
+  data(AirPassengers)
+  p <- as.numeric(p)
+  d <- as.numeric(d)
+  q <- as.numeric(q)
+  P <- as.numeric(P)
+  D <- as.numeric(D)
+  Q <- as.numeric(Q)
+  h <- as.numeric(h)
+  
+  md <- arima(AirPassengers, order = c(p,d,q), seasonal = list(order = c(P,D,Q)))
+  fc <- forecast::forecast(md, h = h)
+  p <- plot(fc)
+
+}
+
+
+

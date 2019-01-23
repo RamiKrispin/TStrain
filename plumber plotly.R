3ed5b878a744d71bd966f164f992ffbf8f00d24e
RamiKrispin <ramkrisp@umich.edu>
2019-01-22 21:55:19 -0800

    Testing plotly on plumber

---
+++ plumber plotly.R
@@ -0,0 +1,28 @@
+# plumber.R
+
+#* Forecast with ARIMA model the AirPassenger dataset
+#* @param p:int
+#* @param d:int
+#* @param q:int
+#* @param P:int
+#* @param D:int
+#* @param Q:int
+#* @param h:int
+#* @get /plotly
+#* @serializer htmlwidget
+function(p = 0, d = 0, q = 0, P = 0, D = 0 ,Q = 0, h = 12){
+  data(AirPassengers)
+  p <- as.numeric(p)
+  d <- as.numeric(d)
+  q <- as.numeric(q)
+  P <- as.numeric(P)
+  D <- as.numeric(D)
+  Q <- as.numeric(Q)
+  h <- as.numeric(h)
+  
+  md <- arima(AirPassengers, order = c(p,d,q), seasonal = list(order = c(P,D,Q)))
+  fc <- forecast::forecast(md, h = h)
+  p <- TSstudio::plot_forecast(fc)
+  print(p)
+  
+}

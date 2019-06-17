bb84e7da20246e781fa13fdbf7bac9e96f6ea445
RamiKrispin <ramkrisp@umich.edu>
2019-06-17 01:28:27 -0700

    Add seasonal component for monthly and quarterly seasonality

--- lm model.R
+++ lm model.R
@@ -1,3 +1,4 @@
+rm(list  = ls())
 library(TSstudio)
 library(tsibble)
 
@@ -7,7 +8,13 @@ data("USgas")
 
 input <- as_tsibble(USgas)
 tsibble::is_regular(input)
-freq <- tsibble::interval(input)
+
+# Frequency option
+# Quarterly series -> quarter
+# Monthly series -> month, quarter
+# Weekly series -> week, month, and quarter
+# Daily series -> day of the year, day of the week, week, month, and quarter
+# Hourly series -> hour, day of the week, day of the year, day of the week, week, month, and quarter
 
 
 input <- USgas
@@ -15,17 +22,17 @@ input <- USgas
 ts_reg <- function(input, 
                 y, 
                 x = NULL, 
-                seasonal = FALSE, 
-                trend = FALSE, 
+                seasonal = NULL, 
+                trend = TRUE, 
                 ar = NULL, 
-                method = "lm",
+                method =  "lm", 
                 method_arg = list(step = FALSE),
                 scale = NULL){
   
   freq <- NULL
+  freq <- base::names(base::which(purrr::map(tsibble::interval(df), ~.x) == 1))
   
-  
-  
+  # Setting the input table
   if(any(class(input) == "tbl_ts")){
     df <- input
   } else if(any(class(input) == "ts")){
@@ -35,44 +42,50 @@ ts_reg <- function(input,
       warning("The 'x' argument cannot be used when input is a 'ts' class")
     }
     x <- NULL
-
   }
   
-  freq <- base::names(base::which(purrr::map(tsibble::interval(df), ~.x) == 1))
-  if(seasonal){
-    if("month" %in% freq){
-      df$month <- lubridate::month(df$index, label = TRUE) %>% base::factor(ordered = FALSE)
-      x <- c(x, "month")
-    } else if("quarter" %in% freq){
-      df$quarter <- lubridate::quarter(df$index, label = TRUE) %>% base::factor(ordered = FALSE)
-      x <- c(x, "quarter")
-    } else if("week" %in% freq){
-      df$quarter <- lubridate::week(df$index) %>% base::factor(ordered = FALSE)
-      x <- c(x, "week")
-    } 
-  }
-  
-  if(trend){
-    df$trend <- 1:nrow(df)
-    x <- c(x, "trend")
-  }
-  
-  if(!base::is.null(ar)){
-    if(!base::is.numeric(ar)){
-      warning("The 'ar' argument is not valid and will be ignored")
-    } else if(base::any((ar %% 1) > 0 )){
-      warning("The 'ar' argument is not valid and will be ignored")
-    } else {
-    
-      for(lag in ar){
-        df[paste("lag", lag, sep = "")] <- dplyr::lag(df$y, n = lag)
+  # Setting the frequency component
+  if(!base::is.null(seasonal)){
+    if(freq == "quarter"){
+      if(base::length(seasonal) == 1 & seasonal == "quarter"){
+        df$quarter <- lubridate::quarter(df$index) %>% base::factor(ordered = FALSE)
+        x <- c(x, "quarter")
+      } else if(base::length(seasonal) > 1 & "quarter" %in% seasonal){
+        warning("Only quarter seasonal component can be used with quarterly frequency")
+        df$quarter <- lubridate::quarter(df$index) %>% base::factor(ordered = FALSE)
+        x <- c(x, "quarter")
+      } else {
+        stop("The seasonal component is not valid")
+      }
+    } else if(freq == "month"){
+      if(base::length(seasonal) == 1 && seasonal == "month"){
+        df$month <- lubridate::month(df$index, label = TRUE) %>% base::factor(ordered = FALSE)
+        x <- c(x, "month")
+      } else if(all(seasonal %in% c("month", "quarter"))){
+        df$quarter <- lubridate::quarter(df$index) %>% base::factor(ordered = FALSE)
+        df$month <- lubridate::month(df$index, label = TRUE) %>% base::factor(ordered = FALSE)
+        x <- c(x, "month", "quarter")
+      } else if(any(seasonal %in% c("month", "quarter"))){
+        if("month" %in% seasonal){
+          df$month <- lubridate::month(df$index, label = TRUE) %>% base::factor(ordered = FALSE)
+          x <- c(x, "month")
+        } 
         
-        x <- c(x, paste("lag", lag, sep = ""))
+        if("quarter" %in% seasonal){
+          df$quarter <- lubridate::quarter(df$index) %>% base::factor(ordered = FALSE)
+          x <- c(x, "quarter")
+        }
+        
+        warning("For monthly frequency only 'month' or 'quarter' seasonal component could be used")
       }
     }
+  }
       
-      
-    }
+  
+  f <- stats::as.formula(paste("y ~ ", paste0(x, collapse = " + ")))
+  md <- stats::lm(f, data = df)
+  
+  
   }
   
   
